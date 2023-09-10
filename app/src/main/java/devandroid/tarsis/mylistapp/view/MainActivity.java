1b02a46aec36c3db2cf601aed7e39361de9821bb
TarkosJordan <tarsis.orsi@gmail.com>
2023-09-10 02:52:22 -0300

    Implemetação de comportamento para botões.

--- app/src/main/java/devandroid/tarsis/mylistapp/view/MainActivity.java
+++ app/src/main/java/devandroid/tarsis/mylistapp/view/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.tarsis.mylistapp.R;
 
@@ -33,15 +35,51 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setEmail("tarkos.jordan@gmail.com");
         pessoa.setNumeroTelefone("55-54-91321020");
 
+        Pessoa pessoaDoCadastro = new Pessoa();
+
         editNome = findViewById(R.id.editNome);
         editSobrenome = findViewById(R.id.editSobrenome);
         editTelefone = findViewById(R.id.editTelefone);
         editEmail = findViewById(R.id.editEmail);
 
+        buttonLimpar = findViewById(R.id.buttonLimpar);
+        buttonFinalizar = findViewById(R.id.buttonFinalizar);
+        buttonSalvar = findViewById(R.id.buttonSalvar);
+
         editNome.setText(pessoa.getPrimeiroNome());
         editSobrenome.setText(pessoa.getSobrenome());
         editTelefone.setText(pessoa.getNumeroTelefone());
         editEmail.setText(pessoa.getEmail());
+
+        buttonLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editNome.setText("");
+                editSobrenome.setText("");
+                editEmail.setText("");
+                editTelefone.setText("");
+            }
+        });
+
+        buttonFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Aplicativo Encerrado", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoaDoCadastro.setPrimeiroNome(editNome.getText().toString());
+                pessoaDoCadastro.setSobrenome(editSobrenome.getText().toString());
+                pessoaDoCadastro.setNumeroTelefone(editTelefone.getText().toString());
+                pessoaDoCadastro.setEmail(editEmail.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo "+pessoaDoCadastro.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
 
         Log.i("POOAndroid", pessoa.toString());
     }
